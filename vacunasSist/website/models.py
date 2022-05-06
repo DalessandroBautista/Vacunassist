166cded0fc553aa8bc635eb351f871cefbd3f182
DalessandroBautista <dalessandrobau@gmail.com>
2022-05-06 00:01:54 -0300

    arrancando el index

--- vacunasSist/website/models.py
+++ vacunasSist/website/models.py
@@ -4,12 +4,21 @@ from django.contrib.auth.models import User
 
 
 class Vacuna(models.Model):
-    nombre= models.CharField(max_length=100)
-    informacion= models.CharField(max_length=700)
+    nombre=models.CharField(max_length=40)
+    informacion= models.TextField(max_length=700)
+    class Meta:
+        verbose_name='vacuna'
+        verbose_name_plural='vacunas'
+    
+    def __str__(self):
+        return self.nombre
+
+
+
 
 class Vacunatorio(models.Model):
-    nombre=models.CharField()
-    ubicacion=models.CharField()
+    nombre=models.CharField(max_length=40)
+    ubicacion=models.CharField(max_length=40)
     
 """ acá podemos hacer modelo de persona y que usuario admin y vacunador hereden lo básico"""
 
@@ -21,9 +30,8 @@ class Usuario(models.Model):
     dni=models.IntegerField(max_length=8, help_text="DNI de la persona")
     residencia = models.TextField(help_text="Descripción de la tarea")
     historial_vacunas = models.ForeignKey(User, on_delete=models.CASCADE)
-    id_vacunas_aplicadas = models.ForeignKey(Vacuna)
+    id_vacunas_aplicadas = models.ForeignKey(Vacuna, on_delete=models.CASCADE)
     fecha_creacion = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.nombre
-
