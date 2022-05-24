4d0b95ba395f894e1cbad0c02bdac07c1cb8338d
leandro230599 <51353353+leandro230599@users.noreply.github.com>
2022-05-24 11:49:10 -0300

    Update and rename 0013_rename_user_turno_username.py to 0014_rename_user_turno_username.py

--- vacunasSist/website/migrations/0014_rename_user_turno_username.py
+++ vacunasSist/website/migrations/0014_rename_user_turno_username.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('website', '0012_remove_usuario_vacunas_aplicadas_vacunadeusuario.py'),
+        ('website', '0013_remove_usuario_vacunas_aplicadas_vacunadeusuario.py'),
     ]
 
     operations = [
