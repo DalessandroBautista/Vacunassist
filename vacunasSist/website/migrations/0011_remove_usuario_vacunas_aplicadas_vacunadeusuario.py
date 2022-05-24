5cafd4720fc6739971b5347020da9cca4455e852
leandro230599 <51353353+leandro230599@users.noreply.github.com>
2022-05-24 11:45:15 -0300

    Update and rename 0010_remove_usuario_vacunas_aplicadas_vacunadeusuario.py to 0011_remove_usuario_vacunas_aplicadas_vacunadeusuario.py

--- vacunasSist/website/migrations/0011_remove_usuario_vacunas_aplicadas_vacunadeusuario.py
+++ vacunasSist/website/migrations/0011_remove_usuario_vacunas_aplicadas_vacunadeusuario.py
@@ -8,7 +8,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('website', '0009_estadosturno_and_more'),
+        ('website', '0010_estadosturno_and_more.py'),
     ]
 
     operations = [
