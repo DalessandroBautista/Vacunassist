11e0a252ef10b225ae63c56d4142a7e6d3a0cd96
leandro230599 <51353353+leandro230599@users.noreply.github.com>
2022-05-24 11:49:47 -0300

    Update and rename 0011_rename_username_turno_user.py to 0012_rename_username_turno_user.py

--- vacunasSist/website/migrations/0012_rename_username_turno_user.py
+++ vacunasSist/website/migrations/0012_rename_username_turno_user.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('website', 0010_estadosturno_and_more.py'),
+        ('website', 0011_estadosturno_and_more.py'),
     ]
 
     operations = [
