2e16b76f78a73dd4c34649aaf58b5a1477ea0409
leandro230599 <51353353+leandro230599@users.noreply.github.com>
2022-05-24 11:48:53 -0300

    Update and rename 0014_historial_vacunacion_and_more.py to 0015_historial_vacunacion_and_more.py

--- vacunasSist/website/migrations/0015_historial_vacunacion_and_more.py
+++ vacunasSist/website/migrations/0015_historial_vacunacion_and_more.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('website', '0013_rename_username_turno_user.py'),
+        ('website', '0014_rename_username_turno_user.py'),
     ]
 
     operations = [
