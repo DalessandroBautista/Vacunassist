d3d5f824e132ff089785a6723f9c83425899bfad
leandro230599 <51353353+leandro230599@users.noreply.github.com>
2022-05-24 11:44:44 -0300

    Update and rename 0009_estadosturno_and_more.py to 0010_estadosturno_and_more.py

--- vacunasSist/website/migrations/0010_estadosturno_and_more.py
+++ vacunasSist/website/migrations/0010_estadosturno_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('website', '0008_turno'),
+        ('website', '0009_alter_turno_fecha.py'),
     ]
 
     operations = [
