9dcd3d1d046d8285655d4b0d2f055b7fadb606cd
DalessandroBautista <dalessandrobau@gmail.com>
2022-06-11 23:53:10 -0300

    contraseña email

--- vacunasSist/vacunasSist/settings.py
+++ vacunasSist/vacunasSist/settings.py
@@ -100,7 +100,7 @@ EMAIL_HOST="smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
 EMAIL_HOST_USER="vacunassist2022@gmail.com"
-EMAIL_HOST_PASSWORD="Clave.123"
+EMAIL_HOST_PASSWORD="ospozuetdpgxayqm"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
