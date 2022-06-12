00e3498603c17e94dccace2b2cd500334d022221
DalessandroBautista <dalessandrobau@gmail.com>
2022-06-11 23:14:45 -0300

    Rol vacunador e if en

--- vacunasSist/website/models.py
+++ vacunasSist/website/models.py
@@ -22,7 +22,7 @@ class Vacunatorio(models.Model):
     nombre=models.CharField(max_length=40)
     ubicacion=models.CharField(max_length=40)
     def __str__(self):
-       return self.nombre + self.ubicacion
+           return self.nombre + self.ubicacion
 """ acá podemos hacer modelo de persona y que usuario admin y vacunador hereden lo básico"""
 
 class UsuarioManager(BaseUserManager):
@@ -61,33 +61,35 @@ class Usuario(AbstractBaseUser):
     vacunatorio_preferencia =models.ForeignKey(Vacunatorio, on_delete=models.CASCADE,null=True)
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     identidad_verificada=models.BooleanField(default=False)
-    usuario_activo= models.BooleanField(default=True)
-    usuario_administrador = models.BooleanField(default=False)
+    es_vacunador = models.BooleanField(default=False)
+    es_administrador = models.BooleanField(default=False)
     objects= UsuarioManager()
     
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['email','nombre', 'apellido','dni', 'fecha_nacimiento']
     
-class UsuarioConPrivilegios(AbstractBaseUser):
-    username =models.CharField('Nombre de usuario', unique=True, max_length=100)
-    email = models.EmailField('Correo electrónico', max_length=254, unique=True)
-    nombre = models.CharField('Nombre', max_length=254)
-    apellido = models.CharField('apellido', max_length=254)
-    es_vacunador = models.BooleanField(default=False)
-    es_administrador = models.BooleanField(default=False)
-    dni=models.IntegerField(help_text="DNI de la persona", unique=True)
+#class UsuarioConPrivilegios(AbstractBaseUser):
+#    username =models.CharField('Nombre de usuario', unique=True, max_length=100)
+#   email = models.EmailField('Correo electrónico', max_length=254, unique=True)
+#    nombre = models.CharField('Nombre', max_length=254)
+#    apellido = models.CharField('apellido', max_length=254)
+ #   es_vacunador = models.BooleanField(default=False)
+ #   es_administrador = models.BooleanField(default=False)
+ #   dni=models.IntegerField(help_text="DNI de la persona", unique=True)
+
+class EstadosTurno(models.Model):
+    estado= models.CharField(max_length=30)
     
+        
 class Turno(models.Model):
     user = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     vacuna = models.CharField(max_length=20)
     fecha =  models.DateField(null=True)
     asignado = models.BooleanField(default=False)
-    #estado= models.ForeignKey(EstadosTurno, on_delete=models.CASCADE)
+    estado= models.ForeignKey(EstadosTurno, on_delete=models.CASCADE)
 #    Turno.objects.filter(estado=True).filter(user__id=request.user.id)    
 
-class EstadosTurno(models.Model):
-    estado= models.CharField(max_length=30)
-    
+
 class VacunaDeUsuario(models.Model):
     user= models.ForeignKey(Usuario, on_delete=models.CASCADE)
     vacuna= models.ForeignKey(Vacuna, on_delete=models.CASCADE)
